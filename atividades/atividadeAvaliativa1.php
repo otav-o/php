e33c808fa90aa675ce1ed05c8abb1e1b66164ff4
Otávio Dioscânio <otaviocacadordioscanio@gmail.com>
2022-03-22 18:33:00 -0300

    Terminar o layout

--- atividades/atividadeAvaliativa1.php
+++ atividades/atividadeAvaliativa1.php
@@ -15,41 +15,43 @@
     </header>
 
     <main>
-        <div class="mb-3">
-            <label for="nome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="nome" placeholder="Nome">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="Email">
-        </div>
-        <div class="mb-3">
-            <label for="endereco" class="form-label">Endereço</label>
-            <textarea class="form-control" id="endereco" rows="3"></textarea>
-        </div>
+        <form method="POST">
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="nome" placeholder="Nome">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Email">
+            </div>
+            <div class="mb-3">
+                <label for="endereco" class="form-label">Endereço</label>
+                <textarea class="form-control" id="endereco" rows="3"></textarea>
+            </div>
 
-        <select name="ocupacao" class="form-select" aria-label="Selecione seu cargo">
+            <select name="ocupacao" class="form-select" aria-label="Selecione seu cargo">
 
-            <option selected>Ocupação</option>
-            <?php
+                <option selected>Profissão</option>
+                <?php
                 $profissoes = ["Engenheiro de Software", "Analista de Segurança da Informação", "Analista de Sistemas", "Administrador de Sistemas", "Administrador de banco de dados (DBA)", "Gestor de Tecologias da Informação", "Arquiteto de Redes", "Desenvolvedor"];
 
                 for ($i = 0; $i < count($profissoes); $i += 1)
                     echo "<option value='$i'>$profissoes[$i]</option>";
-            ?>
-        </select>
-        <br>
-        <select name="genero" id="genero" class="form-select">
-            <option selected>Gênero</option>
-            <option value="M">Masculino</option>
-            <option value="F">Feminino</option>
-            <option value="P">Prefiro não informar</option>
-            <!-- TODO: inserir "Outro" e input text -->
-        </select>
-        <br>
-        <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-            <p>Área de interesse</p>
-            <?php
+                ?>
+            </select>
+            <br>
+            <select name="genero" id="genero" class="form-select">
+                <option selected>Gênero</option>
+                <option value="M">Masculino</option>
+                <option value="F">Feminino</option>
+                <option value="P">Prefiro não informar</option>
+                <option value="O">Outro</option>
+                <!-- TODO: inserir "Outro" e input text -->
+            </select>
+            <br>
+            <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+                <p>Área de interesse</p>
+                <?php
                 $areasDeInteresse = ["Banco de Dados", "Engenharia de Software", "Redes de Computadores", "Desenvolvimento de Jogos", "Programação back-end", "Programação front-end", "Mineração de Dados"];
 
                 for ($i = 0; $i < count($areasDeInteresse); $i += 1) {
@@ -57,12 +59,47 @@
                     echo "<label class='btn btn-outline-primary' for='$areasDeInteresse[$i]'>$areasDeInteresse[$i]</label>";
                 }
                 ?>
-        </div>
+            </div>
 
+            <h3 class="h3">Fale conosco</h3>
+            <div class="mb-3">
+                <label for="mensagem" class="form-label">Mensagem</label>
+                <textarea class="form-control" id="mensagem" rows="3"></textarea>
+            </div>
+
+            Deseja receber nossas promoções?
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="promocoes" id="simPromocoes">
+                <label class="form-check-label" for="simPromocoes">
+                    Sim
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="promocoes" id="naoPromocoes" checked>
+                <label class="form-check-label" for="naoPromocoes">
+                    Não
+                </label>
+            </div>
+
+
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Senha</span>
+                <input type="password" class="form-control" aria-label="Senha" aria-describedby="inputGroup-sizing-default">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="inputGroup-sizing-default">Confirmar</span>
+                <input type="password" class="form-control" aria-label="Senha" aria-describedby="inputGroup-sizing-default">
+            </div>
+
+            <button type="button" class="btn btn-success">Enviar</button>
+
+        </form>
 
 
     </main>
-    <small>Primeira atividade avaliativa de TADS - Otávio Dioscânio 15/03/2022</small>
+    <footer>
+        <small>Primeira atividade avaliativa de TADS - Otávio Dioscânio 15/03/2022</small>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
